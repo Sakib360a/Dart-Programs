f6aae0878d9c1718c7fa67db2f152c5d9f1b7068
Abu Bakkar Siddique Sakib <abubakkarsakib360@gmail.com>
2025-06-25 20:51:54 +0600

    Ostad Module5 Assignment Completed

---
+++ Dart-BasicPrograms/module 1 class 2.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+main() {
+  // String name = 'Sakib';
+  // int age = 25;
+  // //print('Name: $name and Age: $age');
+  // print('Name: '+name+ ' and Age: '+ age.toString());
+  // String ? name1;
+  // print(name1);
+  // int ? digit= int.tryParse(stdin.readLineSync()!);
+  // print(digit);
+  // print('Enter the 1st number: ');
+  // double? num1= double.parse(stdin.readLineSync()!);
+  // print('Enter the 2nd number: ');
+  // double? num2= double.parse(stdin.readLineSync()!);
+  // print('Sum:  ${num1 + num2}');
+  // print('Sub:  ${num1 - num2}');
+  // print('Mul:  ${num1 * num2}');
+  // print('Div:  ${num1 / num2}');
+  String name  = 'Alice';
+  int age= 25;
+  print("Name: $name, Age: $age");
+  dynamic hello= 5;
+
+}
